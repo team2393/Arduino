d53ebe737392fa8ce69d610eaed0ad96923c6670
unknown <team2393@laptop.org>
2019-09-07 10:44:34 -0400

    Servo

---
+++ Servo/Servo.ino
@@ -0,0 +1,46 @@
+/* Based on File, Examples, Servo, Sweep
+   http://www.arduino.cc/en/Tutorial/Sweep
+
+   Uses servo on pin 9, meaning:
+   Black servo wire to GND,
+   red servo wire to 5V,
+   white (sometimes yellow) wire to pin 9.
+
+   TODO: Does the servo reach 180 degrees?
+   Or do we only get to almost 180?
+
+   TODO: Change the stepping.
+   Command the servo to -90, 0, 90, 0, -90, 0, 90, ...,
+   waiting 2 seconds at each position.
+   Do NOT step -90, -89, -88, ..., 0 but directly command
+   the servo to go to -90, pause, 0, pause, ...
+   to see how fast the servo can reach a setpoint.
+*/
+
+// Get servo library, create servo object
+#include <Servo.h>
+Servo myservo;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Servo on pin 9");
+  // Attach servo to pin 9
+  myservo.attach(9);
+}
+
+void loop()
+{
+  // Go from 0 to (almost) 180 degrees
+  for (int angle = 0; angle < 180; angle += 1)
+  {
+    myservo.write(angle);
+    delay(15);
+  }
+  // Back from 180 to (almost) 0
+  for (int angle = 180; angle > 0; angle -= 1)
+  {
+    myservo.write(angle);
+    delay(15);
+  }
+}
